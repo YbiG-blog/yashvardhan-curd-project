2e2e45ee8f1dd24b5b9ce33928f2e1676a7a4173
yash-blog <yashvardhan7906@gmail.com>
2022-01-27 02:17:01 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@
 require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 require("./datacon/datacon");
 //export modelRout
 // const userRoute = require("./modelRoutes/User");
@@ -14,17 +17,103 @@ const app = express();
 app.use(express.json());
 // app.use("/api/users", userRoute);
 
+/// find all user Data
+app.get("/register", async(req, res)=>{
+  
+  try{
+const userData = await User.find();
+res.send(userData);
+  }catch(err)
+  {
+  res.status(400).send(err); 
+  }
 
-
-app.post("/register",(req, res) => {
-  console.log(req.body);
-  const newUser = new User(req.body);
-  newUser.save().then(()=>{
-    res.status(201).send(newUser);
-  }).catch((err)=>{
-res.status(400).send(err);
-  })
 });
+app.get("/register/:userId", async (req, res)=> {
+  try{
+    const _id= req.params.userId;
+    const singleUser = await User.findById(_id);
+if(!singleUser)
+{
+ return res.status(404).send();
+}
+else
+{
+  res.send(singleUser);
+}
+      }catch(err)
+      {
+      res.status(400).send(err); 
+      }
+    const tittlename= req.params.userId;
+  })
+
+app.post("/register", async(req, res) => {
+    try{
+const password = req.body.password;
+const confirmpassword = req.body.confirmpassword;
+if(password===confirmpassword){
+    const newUser = new User({
+      name: req.body.name,
+  email: req.body.email,
+  rollnum: req.body.rollnum,
+  password: password,
+  phone: req.body.phone,
+  confirmpassword:confirmpassword
+    });
+    console.log(newUser);
+    const token = await newUser.generateAuthToken();
+  console.log("token is here -> "+token)
+    const creatUser = await newUser.save();
+    res.status(201).send(creatUser);
+  }
+  else{
+    res.send("password are not matching")
+  }
+  }catch(err){
+  res.status(400).send(err);
+    }
+
+
+});
+
+
+/// login page
+
+app.post("/login", async(req,res)=>{
+  try{
+const email = req.body.email;
+const password = req.body.password;
+const useremail = await User.findOne({email:email});
+if(!useremail){
+  return  res.status.send("Email or password is not valid");
+}
+
+const matchPassword = await bcrypt.compare(password,useremail.password);
+const token = await useremail.generateAuthToken();
+  console.log("token is here -> "+token)
+if(matchPassword){ return  res.status(201).send("login successfully");
+}
+else{
+  res.send("Wrong password");
+}
+
+  }catch(err){
+  res.status(400).send("Invalid details");
+    }
+    
+
+});
+
+
+// const createToken= async()=>{
+//  const token = await jwt.sign({_id:"61f1882eb8fc6208a57ec0fe"},process.env.JWT_SECRET,{expiresIn: "3 minutes"});
+//  console.log(token);
+
+//  const userverify= await jwt.verify(token,process.env.JWT_SECRET);
+//  console.log(userverify)
+// }
+// createToken();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
--- server/modelUser/User.js
+++ server/modelUser/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -32,9 +35,44 @@ throw new Error("username is not Valid")
     min: 10,
     unique: true,
     required: true
-  }
+  },
+  confirmpassword: {
+    type: String,
+    required: true
+  },
+  tokens:[{
+    token:{
+      type: String,
+    required: true
+    }
+  }]
 });
+/// generate token
+UserSchema.methods.generateAuthToken = async function(){
+  try{
+    const token = jwt.sign({_id:this._id.toString()},process.env.JWT_SECRET);
+   this.tokens=this.tokens.concat({token:token})
+await this.save();
+return token;    
+    //  const userverify= await jwt.verify(token,process.env.JWT_SECRET);
+    //  console.log(userverify)
 
+  }catch(err)
+  {
+res.send(err);
+  }
+}
+
+// authenticate password
+UserSchema.pre("save", async function(next){
+  if(this.isModified("password")){
+    // console.log(this.password)
+    this.password= await bcrypt.hash(this.password,saltRounds);
+    // console.log(this.password)
+    this.confirmpassword = await bcrypt.hash(this.password,saltRounds);
+  }
+  next();
+})
 const User = new mongoose.model("User",UserSchema)
 module.exports = User;
 
