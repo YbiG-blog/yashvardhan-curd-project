9a547d2756f65cbdff387739a8f2d41509c4965a
yash-blog <yashvardhan7906@gmail.com>
2022-01-28 16:13:43 +0530

    .

--- server/datacon/datacon.js
+++ server/datacon/datacon.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_KEY,{
+mongoose.connect("mongodb+srv://admin-yash:yash2121@cluster0.6afcs.mongodb.net/csifinalprojectDB",{
   useNewUrlParser: true,
 }).then(()=>{
   console.log("Connection with database is successfully")
